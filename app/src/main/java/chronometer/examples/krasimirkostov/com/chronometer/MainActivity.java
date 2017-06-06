7fdddd4a0e9176c70b415e7d7f067a2b2f41643c
kkostov5 <k_kostov95@abv.bg>
2017-06-06 15:27:26 +0100

    Finished with the menu

--- app/src/main/java/chronometer/examples/krasimirkostov/com/chronometer/MainActivity.java
+++ app/src/main/java/chronometer/examples/krasimirkostov/com/chronometer/MainActivity.java
@@ -1,128 +1,178 @@
 package chronometer.examples.krasimirkostov.com.chronometer;
 
-import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ScrollView;
-import android.widget.TabHost;
-import android.widget.TabWidget;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView timeText;
-    private TextView et_laps;
-    private ScrollView sv_Laps;
-    private Button btnStart;
-    private Button btnLap;
-    private Button btnStop;
-    private TabHost tabHost;
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    private int laps;
-    private Context context;
-    private Chronometer chrono;
-    private Thread threadChrono;
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        et_laps = (TextView) findViewById(R.id.et_laps);
-        timeText = (TextView) findViewById(R.id.displayTime);
-        sv_Laps = (ScrollView) findViewById(R.id.sv_laps);
-        btnStart = (Button) findViewById(R.id.btn_start);
-        btnLap = (Button) findViewById(R.id.btn_lap);
-        btnStop = (Button) findViewById(R.id.btn_stop);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        tabHost = (TabHost) findViewById(R.id.tabhost);
-        btnStop.setEnabled(false);
-        context = this;
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        tabHost.setup();
-
-        TabHost.TabSpec spec = tabHost.newTabSpec("Alarms");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Alarms");
-        tabHost.addTab(spec);
-
-         spec = tabHost.newTabSpec("Timer");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("Timer");
-        tabHost.addTab(spec);
-
-         spec = tabHost.newTabSpec("World Clock");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("World Clock");
-        tabHost.addTab(spec);
-
-         spec = tabHost.newTabSpec("Stopwatch");
-        spec.setContent(R.id.tab4);
-        spec.setIndicator("Stopwatch");
-        tabHost.addTab(spec);
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Start button is pressed
-                if(chrono == null)
-                {
-                    chrono = new Chronometer(context);
-                    threadChrono = new Thread(chrono);
-                    threadChrono.start();
-                    chrono.start();
-                    laps = 1;
-                    btnStop.setEnabled(true);
-                }
-            }
-        });
-
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(chrono != null)
-                {
-                    chrono.stop();
-                    threadChrono.interrupt();
-                    threadChrono = null;
-                    chrono = null;
-                    et_laps.setText("");
-                }
-            }
-        });
-
-        btnLap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(chrono != null)
-                {
-                    et_laps.append("LAP "+String.valueOf(laps)+" : " + String.valueOf(timeText.getText()) + "\n");
-
-                    sv_Laps.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            sv_Laps.smoothScrollBy(0,et_laps.getBottom());
-                        }
-                    });
-                    laps++;
-
-                }
-            }
-        });
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
     }
 
-    public void updateTimerText(final String time)
-    {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                timeText.setText(time);
-            }
-        });
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private int position;
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment()
+        {
+
+        }
+
+        public PlaceholderFragment(int position) {
+            this.position = position;
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = null;
+
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+                case 0:
+                    rootView = inflater.inflate(R.layout.activity_alarm, container, false);
+                    break;
+                case 1:
+                    rootView = inflater.inflate(R.layout.activity_timer, container, false);
+                    break;
+                case 2:
+                    rootView = inflater.inflate(R.layout.activity_world_clock, container, false);
+                    break;
+                case 3:
+                    rootView = inflater.inflate(R.layout.activity_stop_watch, container, false);
+                    break;
+            }
+//            View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
+//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 4 total pages.
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Alarms";
+                case 1:
+                    return "Timer";
+                case 2:
+                    return "World clock";
+                case 3:
+                    return "Stop watch";
+            }
+            return null;
+        }
+    }
 }
---
+++ app/src/main/java/chronometer/examples/krasimirkostov/com/chronometer/OldMainActivity.java
@@ -0,0 +1,141 @@
+package chronometer.examples.krasimirkostov.com.chronometer;
+
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
+
+
+public class OldMainActivity extends AppCompatActivity {
+
+    private TextView timeText;
+    private TextView et_laps;
+    private ScrollView sv_Laps;
+    private Button btnStart;
+    private Button btnLap;
+    private Button btnStop;
+    private TabHost tabHost;
+
+    private int laps;
+    private Context context;
+    private Chronometer chrono;
+    private Thread threadChrono;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+//        et_laps = (TextView) findViewById(R.id.et_laps);
+//        timeText = (TextView) findViewById(R.id.displayTime);
+//        sv_Laps = (ScrollView) findViewById(R.id.sv_laps);
+//        btnStart = (Button) findViewById(R.id.btn_start);
+//        btnLap = (Button) findViewById(R.id.btn_lap);
+//        btnStop = (Button) findViewById(R.id.btn_stop);
+
+        tabHost = (TabHost) findViewById(R.id.tabhost);
+//        btnStop.setEnabled(false);
+        context = this;
+
+        tabHost.setup();
+
+        TabHost.TabSpec spec = tabHost.newTabSpec("Alarms");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("Alarms");
+        tabHost.addTab(spec);
+
+
+         spec = tabHost.newTabSpec("Timer");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Timer");
+        tabHost.addTab(spec);
+
+         spec = tabHost.newTabSpec("World Clock");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("World Clock");
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("Stopwatch");
+        spec.setContent(new Intent(getApplicationContext(),StopWatchActivity.class));
+        spec.setIndicator("Stopwatch");
+//        tabHost.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(),StopWatchActivity.class));
+//            }
+//        });
+        tabHost.addTab(spec);
+
+//        btnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //Start button is pressed
+//                if(chrono == null)
+//                {
+//                    chrono = new Chronometer(context);
+//                    threadChrono = new Thread(chrono);
+//                    threadChrono.start();
+//                    chrono.start();
+//                    laps = 1;
+//                    btnStop.setEnabled(true);
+//                }
+//            }
+//        });
+//
+//        btnStop.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(chrono != null)
+//                {
+//                    chrono.stop();
+//                    threadChrono.interrupt();
+//                    threadChrono = null;
+//                    chrono = null;
+//                    et_laps.setText("");
+//                }
+//            }
+//        });
+//
+//        btnLap.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(chrono != null)
+//                {
+//                    et_laps.append("LAP "+String.valueOf(laps)+" : " + String.valueOf(timeText.getText()) + "\n");
+//
+//                    sv_Laps.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            sv_Laps.smoothScrollBy(0,et_laps.getBottom());
+//                        }
+//                    });
+//                    laps++;
+//
+//                }
+//            }
+//        });
+
+    }
+//
+//    public void updateTimerText(final String time)
+//    {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                timeText.setText(time);
+//            }
+//        });
+//    }
+
+}
